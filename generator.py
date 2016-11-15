d34e3bd96ce1a077d23edf4fdcb9d8970c597213
candyolivia <candyoliviamawalim@gmail.com>
2016-11-16 00:00:12 +0700

    highlight-contained string matching

--- generator.py
+++ generator.py
@@ -9,33 +9,44 @@ import os
 java_path = "C:/Program Files/Java/jdk1.8.0_101/bin/java.exe"
 os.environ['JAVAHOME'] = java_path
 
-stopset = set(stopwords.words('english'))
-
 depParser=StanfordDependencyParser(model_path="edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz")
 
 topTerm = {}
 
 with open('rottentomatoes.txt', 'r') as text_file:
     lines = text_file.readlines()
-    # text = re.sub('[^0-9a-zA-Z\s:]+', '', text)
     for text in lines:
     	sentences = sent_tokenize(text.strip())
     	for sentence in sentences:
 	    dependencyTree = [list(parse.triples()) for parse in depParser.raw_parse(sentence)]
 	    for item in dependencyTree[0]:
 	    	if (item[1] == 'amod'):
-	    		if item not in topTerm:
-	    			topTerm[item] = 1
+	    		newitem = (item[0][0],item[2][0])
+	    		if newitem not in topTerm:
+	    			topTerm[newitem] = 1
 	    		else:
-	    			topTerm[item] = topTerm[item] + 1
+	    			topTerm[newitem] = topTerm[newitem] + 1
 
 topTerm = sorted(topTerm.items(), key=operator.itemgetter(1))[-10:]
 
-listTopTerm = []
+listTopReviews = []
 
 for term in topTerm:
-	string = term[0][2][0] + " " + term[0][0][0]
-	print string
+	string = term[0][1] + " " + term[0][0]
+	listTopReviews.append(string)
 
-print topTerm
+seq = {key: [] for key in listTopReviews}
 
+for line in lines:
+	for highlight in listTopReviews:
+		tmp = highlight.split(" ")
+		if (tmp[0] in line and tmp[1] in line):
+			seq[highlight].append(line)
+
+print seq
+# print "\n"
+
+# for highlight in listTopReviews:
+# 	print highlight + "\n"
+# 	for review in seq[highlight]:
+# 		print review
