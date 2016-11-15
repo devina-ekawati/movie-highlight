b585d460c8656ac141e5f821001ba4b52ddc573e
Feryandi Nurdiantoro <feryandi.n@gmail.com>
2016-11-15 20:31:03 +0700

    Automatically Search Movie by Name

--- MovieCriticSite.py
+++ MovieCriticSite.py
@@ -1,12 +1,12 @@
 from lxml import html
 import requests
-import string
 
 class MovieCriticSite:
 	def __init__(self, name):
 		self.name = name
 		self.critics = {}
 		self.audiences = {}
+		self.search = {}
 
 	def setCritics(self, link, xpath):
 		self.critics['link'] = link
@@ -16,6 +16,10 @@ class MovieCriticSite:
 		self.audiences['link'] = link
 		self.audiences['xpath'] = xpath
 
+	def setSearch(self, link, xpath):
+		self.search['link'] = link
+		self.search['xpath'] = xpath
+
 	def getReview(self, film, page_num, utype):
 		if ( hasattr(self, utype) ):
 			if ( utype == 'critics' ):
@@ -23,13 +27,21 @@ class MovieCriticSite:
 			else:
 				data = self.audiences
 
+			film = self.getFilmID(film)
 			headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
 			page = requests.get(data['link'].replace("$page$", str(page_num)).replace("$film$", film), headers=headers)
 			tree = html.fromstring(page.content)
 
 			reviews = tree.xpath(data['xpath'])
-			# reviews = list(map(str.rstrip, reviews))
+			reviews = list(map(str.rstrip, reviews))
 			reviews = list(filter(None, reviews))
 			return reviews
 
+	def getFilmID(self, film):
+		headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+		page = requests.get(self.search['link'].replace("$film$", film), headers=headers)
+		tree = html.fromstring(page.content)
+
+		filmID = tree.xpath(self.search['xpath'])
+		return filmID
 
--- Scrapper.py
+++ Scrapper.py
@@ -3,22 +3,18 @@ from MovieCriticSite import MovieCriticSite
 rt = MovieCriticSite("Rotten Tomatoes")
 rt.setCritics("https://www.rottentomatoes.com/m/$film$/reviews/?page=$page$", "//div[@class=\"the_review\"]/text()")
 rt.setAudiences("https://www.rottentomatoes.com/m/$film$/reviews/?type=user&page=$page$", '//div[@class="user_review"]/text()[last()]')
+rt.setSearch("https://www.rottentomatoes.com/search/?search=$film$", "substring(//section[@id=\"SummaryResults\"]//ul/li//div[@class=\"poster\"]/a/@href, 4)")
 
 mc = MovieCriticSite("Metacritics")
 mc.setCritics("http://www.metacritic.com/movie/$film$/critic-reviews?page=$page$", "//div[@class=\"summary\"]/a[@class=\"no_hover\"]/text()")
 mc.setAudiences("http://www.metacritic.com/movie/$film$/user-reviews?page=$page$", "//div[@class=\"review_body\"]/span/span[@class=\"blurb blurb_expanded\"]/text()|//div[@class=\"review_body\"]/span/text()")
+mc.setSearch("http://www.metacritic.com/search/all/$film$/results", "substring(//li[@class=\"result first_result\"]//a/@href, 8)")
 
 imdb = MovieCriticSite("IMDB")
 #IMDB ga punya page khusus critics :(
 imdb.setAudiences("http://www.imdb.com/title/$film$/reviews?start=$page$", "//div[@id=\"tn15content\"]//div/h2/text()|//div[@class=\"review_body\"]/span/text()")
+imdb.setSearch("http://www.imdb.com/find?ref_=nv_sr_fn&q=$film$&s=all", "substring((//table[@class=\"findList\"])[1]/tr[@class=\"findResult odd\"][1]/td[@class=\"primary_photo\"]/a/@href, 8, 9)")
 
-# print('Reviews: ', imdb.getReview('tt1211837', 0, 'audiences'))
-print('Reviews: ', mc.getReview('doctor-strange', 1, 'audiences'))
-# print('Reviews: ', rt.getReview('doctor_strange_2016', 1, 'audiences'))
-
-# with open("rottentomatoes.txt", "w") as text_file:
-# 	for line in rt.getReview('doctor_strange_2016', 1, 'audiences'):
-# 		if (line.strip() != ''):
-# 			text_file.write(line.encode('utf-8').strip() + "\n")
-# text_file.close()
-
+print('Reviews: ', imdb.getReview('Doctor Strange', 0, 'audiences'))
+print('Reviews: ', mc.getReview('Doctor Strange', 1, 'audiences'))
+print('Reviews: ', rt.getReview('Doctor Strange', 1, 'audiences'))
