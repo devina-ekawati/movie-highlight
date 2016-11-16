398b2c4c9fa35170f17e3f3c8f21e22e10db54ef
Feryandi Nurdiantoro <feryandi.n@gmail.com>
2016-11-16 19:28:56 +0700

    Fix Encode Error

--- MovieCriticSite.py
+++ MovieCriticSite.py
@@ -30,7 +30,7 @@ class MovieCriticSite:
 			film = self.getFilmID(film)
 			headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
 			page = requests.get(data['link'].replace("$page$", str(page_num)).replace("$film$", film), headers=headers)
-			tree = html.fromstring(page.content)
+			tree = html.fromstring((page.content).decode('cp1252').encode('utf-8'))
 
 			reviews = tree.xpath(data['xpath'])
 			reviews = list(map(str.rstrip, reviews))
