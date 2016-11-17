19d0de275c3c76048a3884af9aa1bc4b6ab94d7a
Feryandi Nurdiantoro <feryandi.n@gmail.com>
2016-11-17 16:46:17 +0700

    Using Spacey

---
+++ Sp.py
@@ -0,0 +1,51 @@
+import spacy
+from spacy.en import English
+from spacy.symbols import amod, NOUN
+from MovieCriticSite import MovieCriticSite
+
+rt = MovieCriticSite("Rotten Tomatoes")
+rt.setCritics("https://www.rottentomatoes.com/m/$film$/reviews/?page=$page$", "//div[@class=\"the_review\"]/text()")
+rt.setAudiences("https://www.rottentomatoes.com/m/$film$/reviews/?type=user&page=$page$", '//div[@class="user_review"]/text()[last()]')
+rt.setSearch("https://www.rottentomatoes.com/search/?search=$film$", "substring(//section[@id=\"SummaryResults\"]//ul/li//div[@class=\"poster\"]/a/@href, 4)")
+
+mc = MovieCriticSite("Metacritics")
+mc.setCritics("http://www.metacritic.com/movie/$film$/critic-reviews?page=$page$", "//div[@class=\"summary\"]/a[@class=\"no_hover\"]/text()")
+mc.setAudiences("http://www.metacritic.com/movie/$film$/user-reviews?page=$page$", "//div[@class=\"review_body\"]/span/span[@class=\"blurb blurb_expanded\"]/text()|//div[@class=\"review_body\"]/span/text()")
+mc.setSearch("http://www.metacritic.com/search/all/$film$/results", "substring(//li[@class=\"result first_result\"]//a/@href, 8)")
+
+imdb = MovieCriticSite("IMDB")
+#IMDB ga punya page khusus critics :(
+imdb.setAudiences("http://www.imdb.com/title/$film$/reviews?start=$page$", "//div[@id=\"tn15content\"]//div/h2/text()|//div[@class=\"review_body\"]/span/text()")
+imdb.setSearch("http://www.imdb.com/find?ref_=nv_sr_fn&q=$film$&s=all", "substring((//table[@class=\"findList\"])[1]/tr[@class=\"findResult odd\"][1]/td[@class=\"primary_photo\"]/a/@href, 8, 9)")
+
+def get_highlight(doc):
+	for possible_adjective in doc:
+		#print possible_adjective.orth_, possible_adjective.dep_, possible_adjective.head.pos_
+		if possible_adjective.dep == amod and possible_adjective.head.pos == NOUN:
+			print possible_adjective.orth_, possible_adjective.head.orth_,
+
+			if possible_adjective.head.head.pos == NOUN and possible_adjective.head.orth_ != possible_adjective.head.head.orth_:
+				print possible_adjective.head.head.orth_
+
+			print
+			#verbs.add(possible_adjective.head)
+
+nlp = English()
+
+reviews = rt.getReview('Doctor Strange', 1, 'audiences')
+
+for review in reviews:
+	doc = nlp(unicode(review))
+	get_highlight(doc)
+
+reviews = mc.getReview('Doctor Strange', 1, 'audiences')
+
+for review in reviews:
+	doc = nlp(unicode(review))
+	get_highlight(doc)
+
+reviews = imdb.getReview('Doctor Strange', 0, 'audiences')
+
+for review in reviews:
+	doc = nlp(unicode(review))
+	get_highlight(doc)
