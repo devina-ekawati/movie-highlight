94aa662e8b4530e74c8f8d1d596543b7fdf7f6ab
Feryandi Nurdiantoro <feryandi.n@gmail.com>
2016-11-24 23:56:37 +0700

    Fix Python2

--- TopReviewGenerator.py
+++ TopReviewGenerator.py
@@ -79,24 +79,34 @@ class TopReviewGenerator:
         reviews = [];
         for i in range(0,totalpage):
             for line in imdb.getReview(title, i*10, 'audiences'):
-                if (self.is_ascii(line.encode('utf-8'))):
+                if (is_ascii(str(line)) and not hasNumbers(str(line))):
                     reviews.append(line.encode("utf-8").strip())
             for line in rt.getReview(title, i, 'audiences'):
-                if (self.is_ascii(line.encode('utf-8'))):
+                if (is_ascii(str(line)) and not hasNumbers(str(line))):
                     reviews.append(line.encode("utf-8").strip())
             for line in mc.getReview(title, i, 'audiences'):
-                if (self.is_ascii(line.encode('utf-8'))):
+                if (is_ascii(str(line)) and not hasNumbers(str(line))):
                     reviews.append(line.encode("utf-8").strip())
             for line in rt.getReview(title, i, 'critics'):
-                if (self.is_ascii(line.encode('utf-8'))):
+                if (is_ascii(str(line)) and not hasNumbers(str(line))):
                     reviews.append(line.encode("utf-8").strip())
-        for line in mc.getReview(title, 0, 'critics'):
-            if (self.is_ascii(line.encode('utf-8'))):
-                reviews.append(line.encode("utf-8").strip())
+            for line in mc.getReview(title, i, 'critics'):
+                if (is_ascii(str(line)) and not hasNumbers(str(line))):
+                    reviews.append(line.encode("utf-8").strip())
         return reviews
 
-    def is_ascii(self, text):
-        return all(c < 128 for c in text)
+    def is_ascii(text):
+        if isinstance(text, unicode):
+            try:
+                text.encode('ascii')
+            except UnicodeEncodeError:
+                return False
+        else:
+            try:
+                text.decode('ascii')
+            except UnicodeDecodeError:
+                return False
+    return True
 
     def hasNumbers(self, inputString):
         return any(char.isdigit() for char in inputString)
--- Web.sh
+++ Web.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
+export HTTP_PROXY=http://167.205.22.102:8800
+export HTTPS_PROXY=http://167.205.22.102:8800
+
 export FLASK_APP=Web.py
 flask run --host=0.0.0.0 --port=5002
